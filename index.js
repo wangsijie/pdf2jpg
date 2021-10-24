43bb77f4310a3845183ca09473ee3881c40ce7c7
wangsijie <i@sijie.wang>
2021-10-24 19:15:04 +0800

    fix: height calc in specific page mode, fixed #11

--- index.js
+++ index.js
@@ -1,90 +1,93 @@
 const pdfjsLib = require("pdfjs-dist/es5/build/pdf.js");
-const { createCanvas } = require('canvas');
+const { createCanvas } = require("canvas");
 
 class CanvasFactory {
-    create(width, height) {
-        const canvas = createCanvas(width, height);
-        const context = canvas.getContext('2d');
-        this.canvas = canvas;
-        this.context = context;
-        return { canvas, context };
-    }
-    reset(obj, width, height) {
-        obj.canvas.width = width;
-        obj.canvas.height = height;
-    }
-    destroy(obj) {
-        obj.canvas.width = 0;
-        obj.canvas.height = 0;
-        obj.canvas = null;
-        obj.context = null;
-    }
+  create(width, height) {
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.context = context;
+    return { canvas, context };
+  }
+  reset(obj, width, height) {
+    obj.canvas.width = width;
+    obj.canvas.height = height;
+  }
+  destroy(obj) {
+    obj.canvas.width = 0;
+    obj.canvas.height = 0;
+    obj.canvas = null;
+    obj.context = null;
+  }
 }
 
 class Draw {
-    constructor(width, height) {
-        this.pages = [];
-        this.width = width;
-        this.height = height;
+  constructor(width, height) {
+    this.pages = [];
+    this.width = width;
+    this.height = height;
+  }
+  init() {
+    this.canvas = createCanvas(this.width, this.height);
+    this.context = this.canvas.getContext("2d");
+  }
+  draw() {
+    this.init();
+    let currentY = this.height;
+    for (const page of this.pages.reverse()) {
+      currentY -= page.height;
+      const x = (this.width - page.width) / 2;
+      const y = currentY;
+      this.context.drawImage(page, x, y);
     }
-    init() {
-        this.canvas = createCanvas(this.width, this.height);
-        this.context = this.canvas.getContext('2d');
-    }
-    draw() {
-        this.init();
-        let currentY = this.height;
-        for (const page of this.pages.reverse()) {
-            currentY -= page.height;
-            const x = (this.width - page.width) / 2;
-            const y = currentY;
-            this.context.drawImage(page, x, y);
-        }
-    }
-    getBuffer() {
-        return this.canvas.toBuffer();
-    }
-    putPage(page) {
-        this.pages.push(page);
-    }
+  }
+  getBuffer() {
+    return this.canvas.toBuffer();
+  }
+  putPage(page) {
+    this.pages.push(page);
+  }
 }
 
 async function app(pdfData, options = {}) {
-    const scale = options.scale || 1;
-    const pageNumber = options.page || 0;
-    const doc = await pdfjsLib.getDocument(pdfData).promise;
-    const pages = [];
-    let width = 0;
-    let height = 0;
-    for (let i = 0; i < doc.numPages; i++) {
-        const page = await doc.getPage(i + 1);
-        const viewport = page.getViewport({ scale });
-        if (viewport.width > width) {
-            width = Math.ceil(viewport.width);
-        }
-        height += Math.ceil(viewport.height);
-        pages.push({
-            page,
-            viewport,
-        });
+  const scale = options.scale || 1;
+  const pageNumber = options.page || 0;
+  const doc = await pdfjsLib.getDocument(pdfData).promise;
+
+  const pages = [];
+
+  for (let i = 0; i < doc.numPages; i++) {
+    if (pageNumber && pageNumber !== i + 1) {
+      continue;
     }
-    const draw = new Draw(width, height);
-    const processPage = async (pageObject) => {
-        const { page, viewport } = pageObject;
-        const canvasFactory = new CanvasFactory();
-        const { canvas, context } = canvasFactory.create(viewport.width, viewport.height);
-        const renderContext = { canvasContext: context, viewport, canvasFactory };
-        await page.render(renderContext).promise;
-        draw.putPage(canvas);
-    }
-    for (let i = 0; i < pages.length; i++) {
-        if (pageNumber && pageNumber !== i + 1) {
-            continue;
-        }
-        await processPage(pages[i]);
-    }
-    draw.draw();
-    return draw.getBuffer();
+    const page = await doc.getPage(i + 1);
+    const viewport = page.getViewport({ scale });
+    const width = Math.ceil(viewport.width);
+    const height = Math.ceil(viewport.height);
+    pages.push({
+      page,
+      viewport,
+      width,
+      height,
+    });
+  }
+
+  const width = Math.max(...pages.map(p => p.width));
+  const height = pages.reduce((p, item) => p + item.height, 0);
+
+  const draw = new Draw(width, height);
+
+  for (const pageItem of pages) {
+    const { page, viewport, width, height } = pageItem;
+    const canvasFactory = new CanvasFactory();
+    const { canvas, context } = canvasFactory.create(width, height);
+    const renderContext = { canvasContext: context, viewport, canvasFactory };
+    await page.render(renderContext).promise;
+    draw.putPage(canvas);
+  }
+
+  draw.draw();
+  return draw.getBuffer();
 }
 
 module.exports = app;
